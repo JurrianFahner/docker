60c0915d772a04decac9e2d2724ed7ba07db73fe
Jurrian Fahner <jurrian.fahner@gmail.com>
2016-09-25 17:00:51 +0200

    Update https-redirect to v2.0
    Extra config options made available

--- https-redirect/Readme.md
+++ https-redirect/Readme.md
@@ -4,3 +4,14 @@ To run the simple https-redirect container: `docker run -p 80:80 -it ensignproje
 To build this container use the following commands:
 - compile the golang source (including libs): `CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o redirect .`
 - build the container: `docker build -t imagename . `
+
+**Since version 2.0** the following environment variables are added to change the behavior of the redirect:
+
+* `HOST` example: `-e HOST=google.com`
+* `REDIRECTPATH` example: `-e REDIRECTPATH=true`, this configuration forwards also the path
+
+Full example:
+
+```docker
+docker run -it -p 80:80 -e HOST=google.nl -e REDIRECTPATH=false --rm ensignprojects/https-redirect:v2.0
+```
--- https-redirect/redirect.go
+++ https-redirect/redirect.go
@@ -1,18 +1,64 @@
 package main
 
- import (
-     "log"
-     "net/http"
- )
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
 
- func redirect(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://" + r.Host, 301)
- }
+var port string
+var host string
+var redirectPath bool
 
- func main() {
-     http.HandleFunc("/", redirect)
-     err := http.ListenAndServe(":80", nil)
-     if err != nil {
-         log.Fatal("ListenAndServe: ", err)
-     }
- }
+func createRedirectURL(r *http.Request) string {
+	var redirectURL = "https://"
+
+	if host == "" {
+		host = r.Host
+	}
+
+	redirectURL = redirectURL + host
+
+	if redirectPath {
+		redirectURL = redirectURL + r.URL.Path
+	}
+
+	return redirectURL
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	var newURL = createRedirectURL(r)
+	http.Redirect(w, r, newURL, 301)
+	fmt.Println(time.Now().Format(time.RFC3339), r.Host, r.URL.Path, "->", newURL, r.UserAgent())
+
+}
+
+func readEnv() {
+	port = os.Getenv("PORT")
+	host = os.Getenv("HOST")
+	redirectPath = false
+
+	var envRedirectPath = os.Getenv("REDIRECTPATH")
+	if envRedirectPath != "" {
+		if strings.ToLower(envRedirectPath) == "true" {
+			redirectPath = true
+		}
+	}
+
+	if port == "" {
+		port = "80" // default
+	}
+}
+
+func main() {
+	readEnv()
+
+	http.HandleFunc("/", redirect)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
